d08327b37622457c1372f47ff287890eff37de4c
zackno23 <chikara.f.yoshida@gmail.com>
2021-09-30 01:11:53 +0900

    gitを教えなくちゃ

--- tairadate.py
+++ tairadate.py
@@ -1,7 +1,10 @@
 '''
 9/25
 後で変数名等リファクタリングが必要
-
+9/29
+１時間目：blitの説明まで→ifによる上下左右への動き
+2時間目：上下左右の移動→押しっぱなし処理
+コミットすることを教える
 '''
 
 import pygame
@@ -15,10 +18,10 @@ player = pygame.image.load("player.png")
 playerX = 100
 playerY = 200
 
-
 running = True
 
 while running:
+    screen.fill((0, 0, 0))
     screen.blit(player, (playerX, playerY))
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -27,11 +30,14 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 playerX += 1
-                screen.fill((255, 0, 0))
+                # screen.fill((255, 0, 0))
             if event.key == K_LEFT:
-                screen.fill((0, 255, 0))
+                playerX -= 1
+                # screen.fill((0, 255, 0))
             if event.key == K_UP:
-                screen.fill((0, 0, 255))
+                playerY -= 1
+                # screen.fill((0, 0, 255))
             if event.key == K_DOWN:
-                screen.fill((0, 0, 0))
+                playerY += 1
+                # screen.fill((0, 0, 0))
     pygame.display.update()
