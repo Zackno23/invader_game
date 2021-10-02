369fa47540e962208e07c561a010a248a96f79f9
zackno23 <chikara.f.yoshida@gmail.com>
2021-10-02 16:00:40 +0900

    10月１日oobuke.py編集

--- oobuke.py
+++ oobuke.py
@@ -1,8 +1,10 @@
-
-
 '''
-9/25
-後で変数名等リファクタリングが必要
+10/1
+pygame.key.get_pressedでの移動まで
+[]playerがはみ出ないようにする
+[]enemyの移動
+[]enemyがはみ出ないように
+[]弾を発射させる
 
 '''
 
@@ -25,19 +27,24 @@ enemyY = 200
 running = True
 
 while running:
+    screen.fill((0, 0, 0))
+    key_presseed = pygame.key.get_pressed()
+    if key_presseed[K_RIGHT]:
+        playerX += 1
+    if key_presseed[K_LEFT]:
+        playerX -= 1
+    screen.blit(player, (playerX, playerY))
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
-        if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
-                screen.fill((255, 0, 0))
-            if event.key == K_LEFT:
-                screen.fill((0, 255, 0))
-            if event.key == K_UP:
-                screen.fill((0, 0, 255))
-            if event.key == K_DOWN:
-                screen.fill((0, 0, 0))
+        # if event.type == KEYDOWN:
+        #     if event.key == K_RIGHT:
+        #         screen.fill((255, 0, 0))
+        #     if event.key == K_LEFT:
+        #         screen.fill((0, 255, 0))
+        #     if event.key == K_UP:
+        #         screen.fill((0, 0, 255))
+        #     if event.key == K_DOWN:
+        #         screen.fill((0, 0, 0))
     pygame.display.update()
-
-
--- test.py
+++ test.py
@@ -1,58 +1,45 @@
 '''
-授業前テスト用ファイル
-'''
-
-
 9/25
 後で変数名等リファクタリングが必要
-[]条件分岐からplayerを動かす
-[]連続して描画されちゃう問題の解決（screen.fillの場所）
-[]pygame.key.get_pressed()でのプレイヤー操作
-[]移動速度の調整と、初期位置の調整
-[]画面内から出ていってしまう問題の解決
-[]敵の表示・移動（左右のみ➛近づいてくる）
+
+'''
+import sys
 
 import pygame
 from pygame.locals import *
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("インベーダーゲーム")
-
+screen.fill((0, 0, 0))
 
 player = pygame.image.load("player.png")
 enemy = pygame.image.load("enemy.png")
 
 playerX = 200
 playerY = 100
-
 enemyX = 100
 enemyY = 200
 
+# 略
 running = True
 
 while running:
     screen.fill((0, 0, 0))
     screen.blit(player, (playerX, playerY))
     screen.blit(enemy, (enemyX, enemyY))
+
+    # 現在押されているキーをリストで返す
     key_pressed = pygame.key.get_pressed()
-
-    # playerの動き
-    if key_pressed[K_LEFT] and playerX > 0:
-        playerX -= 1
-    if key_pressed[K_RIGHT] and playerX < 800 - player.get_width():
+    # 右キーが押されたとき
+    if key_pressed[K_RIGHT] == 1:
+        print(key_pressed)
+        sys.exit()
         playerX += 1
-
-    # enemyの動き
-
+    # 左キーが押されたとき
+    if key_pressed[K_LEFT] == 1:
+        playerX -= 1
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        # if event.type == KEYDOWN:
-            # if event.key == K_RIGHT:
-            #     playerX += 10
-            # if event.key == K_LEFT:
-            #     playerX -= 10
 
     pygame.display.update()
-
-
