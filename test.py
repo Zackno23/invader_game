3099bef9dfc648d88d6e5f6452c937de1fb6b82a
ricky-sensei <axcelcamp.teacher@gmail.com>
2021-10-17 22:41:44 +0900

    10/17

--- test.py
+++ test.py
@@ -13,7 +13,10 @@ pygame.display.set_caption("インベーダーゲーム")
 screen.fill((0, 0, 0))
 
 player = pygame.image.load("player.png")
+
 enemy = pygame.image.load("enemy.png")
+tekitou_surface = pygame.Surface((32, 32))
+tekitou_surface.fill((255, 255, 255))
 
 playerX = 200
 playerY = 100
@@ -25,15 +28,15 @@ running = True
 
 while running:
     screen.fill((0, 0, 0))
+
     screen.blit(player, (playerX, playerY))
     screen.blit(enemy, (enemyX, enemyY))
+    screen.blit(tekitou_surface, (screen.get_width()/2, screen.get_height()/2))
 
     # 現在押されているキーをリストで返す
     key_pressed = pygame.key.get_pressed()
     # 右キーが押されたとき
     if key_pressed[K_RIGHT] == 1:
-        print(key_pressed)
-        sys.exit()
         playerX += 1
     # 左キーが押されたとき
     if key_pressed[K_LEFT] == 1:
